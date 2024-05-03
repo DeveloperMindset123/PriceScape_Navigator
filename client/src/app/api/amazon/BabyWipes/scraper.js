a74d9180449393415814dbd34ea221a0235f5816
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-02 23:17:36 -0400

    fix:additional scraping logic

--- client/src/app/api/amazon/BabyWipes/scraper.js
+++ client/src/app/api/amazon/BabyWipes/scraper.js
@@ -30,8 +30,22 @@ export default amazonScraper;
 import { CheerioCrawler, KeyValueStore } from "crawlee";
 //since we want to export router as a component --> because we didn't specify export default, rather simply export followed by the name of the variable
 import { router } from "./route";
+//import the amazon base url from constants.ts
+import { AMAZON_BASE_URL, labels } from "./constants";
+
 
 const { keyword } = await KeyValueStore.getInput();
 const crawler = new CheerioCrawler({
   requestHandler: router,
 });
+
+await crawler.run([
+    {
+        //turn the keyword into a link we can make a request with --> original link :https://www.amazon.com/s?k=baby+wipes&crid=16XWLUEUWX9V3&sprefix=baby+wipes%2Caps%2C96&ref=nb_sb_noss_1
+        url: `${AMAZON_BASE_URL}/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=${keyword}`,
+        label: labels.START,  //oringally START, now set to label.START
+        userData: {
+            keyword,
+        }
+    }
+])
