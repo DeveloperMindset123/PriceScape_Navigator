119f2723edb2c31b4d88eb023ac98f5242f86819
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-08 21:04:39 -0400

    chore:roughed-out functions, requires further finetuning

--- server/productScraping/amazonProducts/babyWipes.py
+++ server/productScraping/amazonProducts/babyWipes.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import requests 
 import pandas as pd 
+import numpy as np
 
+'''
 # set the product url for the search being made
 product_url_babyWipes = "https://www.amazon.com/s?k=baby+wipes&crid=S9G2H5COZ8YV&sprefix=baby+%2Caps%2C102&ref=nb_sb_ss_ts-doa-p_1_5"
 
@@ -13,10 +15,10 @@ webpage = requests.get(url=product_url_babyWipes, headers=HEADERS)
 
 print("Request code:")
 print(webpage)
-''' --> uncomment this if you want to see the html content of the website
+ --> uncomment this if you want to see the html content of the website
 print("\nHTML Content:")
 print(webpage.content)
-'''
+
 print("Type of data")
 print(type(webpage))
 
@@ -59,7 +61,7 @@ individual_product_price = new_beautiful_soup_response.find("span", attrs={"clas
 #print(individual_product_price)  # --> each print statement serves as a check to see if the product prices can be successfully obtained or not
 
 # now we can retrieve the text element content --> note that due to the nested nature of the span element (refer to the previous print statement to see what the output looks like, we need to resort to calling on find more than once)
-individual_product_price_content = new_beautiful_soup_response.find("span", attrs={"class":"a-price a-text-price a-size-medium apexPriceToPay"}).find("span", attrs={"class":"a-offscreen"}).text
+individual_product_price_content = new_beautiful_soup_response.find("span", attrs={"class":"a-price a-text-price a-size-medium apexPriceToPay"}).find("span", attrs={"class":"a-offscreen"})
 #print(individual_product_price_content) # --> tested and worked
 
 # now we can extract the product's ratings
@@ -69,6 +71,128 @@ individual_product_rating_html = new_beautiful_soup_response.find("span", attrs=
 # we can also extract the specific content within the html if needed
 individual_product_rating_text=new_beautiful_soup_response.find("span", attrs={"class":"a-icon-alt"}).text.strip()
 #print(individual_product_rating_text) # --> tested and worked, able to successfully retrieve the information on the product
+
+'''
+'''Note that above code is just to test and see if everything is working as intended, from this point forward, we will be dynamically scraping data from the website instead'''
+
+def get_title(soup):
+    try:
+        # Outer tag object
+        title=soup.find("span", attrs={"id":'productTitle'})
+        
+        # Inner NavigatableString object
+        title_value=title.text 
+        
+        # Title as a string value
+        title_string=title_value.strip()
+        
+    except AttributeError:
+        # in the case that there's an attribute error, set the title_string to empty
+        title_string=""
+        
+    return title_string
+
+# function to extract product price --> note that this may require further modifications
+def get_price(soup):
+    try:
+        price=soup.find("span", attrs={'id':'priceblock_ourprice'}).string.strip()
+    except AttributeError:
+        try:
+            # Check if there's some kind of deal related price
+            price=soup.find("span", attrs={'id':'priceblock_dealprice'}).string.strip()
+            
+        except:
+            price=""
+            
+    return price
+
+# Function used to extract the product rating
+def get_rating(soup):
+    try:
+        rating=soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
+        
+    except AttributeError:
+        try:
+            rating=soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
+        except:
+            rating=""
+            
+# Function to extract number of user reviews
+def get_review_count(soup):
+    try:
+        review_count=soup.find("span", attrs={'id':'acrCustomerReviewText'}).string.strip()
+        
+    except AttributeError:
+        review_count=""
+    
+    return review_count
+
+# function to extract availabillity status
+def get_availability(soup):
+    try:
+        available=soup.find("div", attrs={'id':'availabillity'})
+        available=available.find("span").string.strip()
+        
+    except AttributeError:
+        available="Not Available"
+        
+    return available
+
+# define what to execute if this script is ran directly while also avoidiing running the script if it were to get imported somewhere else
+if __name__ == "__main__":
+    # add the appropriate user agent that corresponds to your specific device --> go to whatismybrowser.com
+    HEADERS=({'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15','Accept-Language':'en-US, en;q=0.5'})
+    
+    # define and add the webpage url
+    URL="https://www.amazon.com/s?k=baby+wipes&crid=S9G2H5COZ8YV&sprefix=baby+%2Caps%2C102&ref=nb_sb_ss_ts-doa-p_1_5"
+    
+    # make an HTTP request --> using the requests library, first parameter that gets passed in is the URL and the next parameter that gets passed in is the header compoent
+    webpage=requests.get(url=URL, headers=HEADERS)
+    
+    # soup object containing all the data --> use Beautiful Soup to successfully parse all the html content --> while also craeting a instance of Beautful Soup
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    
+    # Fetch links as list of Tag objects --> the find_all method available in Beautuful Soup that allows us to search for HTML or XML elements based on various criteria. They have different behaviors in terms of returning single elements (using the find() method) or a list of elements (using the find_all() method) --> in this case, we are trying to find all the anchor tags
+    links=soup.find_all("a", attrs={'class':'a-link-normal s-no-outline'})
+    
+    # store the links
+    links_list=[]
+    
+    # loop for extracting links from tag objects
+    for link in links:
+        # extract the content of the href attribute within the anchor tag and append it to the links_list list that was initialized to hold all the links to the product
+        links_list.append(link.get('href'))
+        
+    # intialize a dictionary, with the values representing the components that makes up a product and the values containing the corresponding array's information, the subsequent for loop is used to upload the information relevant to each of the keys present within this dictionary
+    d = {"title":[], "price":[], "rating":[], "reviews":[], "availabillity":[]}
+    
+    # loop for extracting product details from each link
+    for link in links_list:
+        # pass in the base link and the corresponding product link to get the complete link
+        new_webpage=requests.get("https://www.amazon.com"+link, headers=HEADERS)
+        
+        # take the raw html obtained from new_webpage and parse it using BeautifulSoup's html parser
+        new_soup=BeautifulSoup(new_webpage.content, "html.parser")
+        
+        # Function calls to display all neccessary product information
+        d['title'].append(get_title(new_soup))
+        d['price'].append(get_price(new_soup))
+        d['rating'].append(get_rating(new_soup))
+        d['reviews'].append(get_review_count(new_soup))
+        d["availabillity"].append(get_availability(new_soup))
+        
+    # convert the dictionary into a dataframe using pandas library
+    amazon_df=pd.DataFrame.from_dict(d)
+    # clean up the product titles by replacing the empty strings wiht null values
+    amazon_df['title'].replace('', np.nan, inplace=True)
+    
+    # drop all the null values within the title column
+    amazon_df = amazon_df.dropna(subset=['title'])
+    amazon_df.to_json("./result/baby_wipes_data.json")
+    amazon_df.to_csv("./result/baby_wipes_data.csv", header=True, index=False)
+        
+
+
 
 
 
